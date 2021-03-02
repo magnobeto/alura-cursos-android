d2799d91adeb3d14dce7e9d72f2efa1dee68b6d2
Roberto Magno <roberto.magno@g.globo>
2021-03-02 12:11:43 -0300

    Refatorando o código

--- Architecture Components/android-tech-news-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
+++ Architecture Components/android-tech-news-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
@@ -1,7 +1,6 @@
 package br.com.alura.technews.ui.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.alura.technews.model.Noticia
 import br.com.alura.technews.repository.NoticiaRepository
