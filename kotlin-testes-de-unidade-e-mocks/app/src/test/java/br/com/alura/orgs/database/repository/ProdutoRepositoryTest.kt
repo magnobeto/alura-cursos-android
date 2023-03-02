2f5a3e0ff876c22c24c1c9a866fd603b59b29c86
roberto.magno <roberto.magno@g.globo>
2023-03-02 01:09:10 -0300

    Implementando mockk de retorno

--- kotlin-testes-de-unidade-e-mocks/app/src/test/java/br/com/alura/orgs/database/repository/ProdutoRepositoryTest.kt
+++ kotlin-testes-de-unidade-e-mocks/app/src/test/java/br/com/alura/orgs/database/repository/ProdutoRepositoryTest.kt
@@ -1,15 +1,33 @@
 package br.com.alura.orgs.database.repository
 
-import android.content.Context
-import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.model.Produto
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
+import java.math.BigDecimal
 
 class ProdutoRepositoryTest {
 
     @Test
     fun salva() {
-        val context = mockk<Context>()
-        ProdutoRepository(AppDatabase.instancia(context).produtoDao())
+        val dao = mockk<ProdutoDao>()
+        val produtoRepository = ProdutoRepository(dao)
+        val produto = Produto(
+            nome = "Banana",
+            descricao = "Banana Prata",
+            valor = BigDecimal("6.99")
+        )
+
+        every {
+            dao.salva(produto)
+        }.returns(Unit)
+
+        produtoRepository.salva(produto)
+
+        verify {
+            dao.salva(produto)
+        }
     }
 }
